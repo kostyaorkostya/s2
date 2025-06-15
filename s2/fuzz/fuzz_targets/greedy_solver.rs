90bd5a1fe88767cd0179ee2d404889bd2d0a0bcd
Kostya <happy.boat9793@fastmail.com>
2025-06-15 14:59:29 +0000

    fix build

--- s2/fuzz/fuzz_targets/greedy_solver.rs
+++ s2/fuzz/fuzz_targets/greedy_solver.rs
@@ -2,28 +2,34 @@
 #[macro_use]
 extern crate libfuzzer_sys;
 extern crate s2;
+use s2::cancellation_flag::NeverCancelled;
 use s2::format::{read_from_string, RowMajorAscii};
-use s2::grid::{copy_and_apply, PlainGrid};
+use s2::grid::{ArrGridRowMajor, GridMutWithDefault};
 use s2::solver::{GreedySolver, Solver};
 use s2::status::{eval_status, SudokuStatus};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(grid) = str::from_utf8(data) {
-        if let Ok(grid) = read_from_string::<_, PlainGrid>(&RowMajorAscii::default(), &grid.trim())
+        if let Ok(grid) =
+            read_from_string::<_, ArrGridRowMajor>(&RowMajorAscii::default(), &grid.trim())
         {
             if let Ok(status) = eval_status(&grid) {
                 match status {
                     SudokuStatus::Complete => (),
                     SudokuStatus::Incomplete => {
-                        let complete = copy_and_apply::<_, PlainGrid, _>(
-                            &grid,
-                            GreedySolver::new().solve::<_, Vec<_>>(&grid).into_iter(),
-                        );
-                        match eval_status(&complete) {
-                            Err(_) | Ok(SudokuStatus::Incomplete) => {
-                                panic!("{:?}\n{:?}", grid, complete)
+                        match GreedySolver::new()
+                            .solve::<_, _, Vec<_>>(&NeverCancelled::new(), &grid)
+                        {
+                            Err(_) => (),
+                            Ok(diff) => {
+                                let complete = ArrGridRowMajor::with_diff(&grid, diff.into_iter());
+                                match eval_status(&complete) {
+                                    Err(_) | Ok(SudokuStatus::Incomplete) => {
+                                        panic!("{:?}\n{:?}", grid, complete)
+                                    }
+                                    Ok(SudokuStatus::Complete) => (),
+                                }
                             }
-                            Ok(SudokuStatus::Complete) => (),
                         }
                     }
                 }

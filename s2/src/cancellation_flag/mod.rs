b9c3318fec85b95260fcde47991c32cbd7f0b1dc
Kostya <happy.boat9793@fastmail.com>
2025-06-15 09:24:41 +0100

    atomic flag

--- s2/src/cancellation_flag/mod.rs
+++ s2/src/cancellation_flag/mod.rs
@@ -5,3 +5,6 @@ pub trait CancellationFlag: Send + Sync {
 mod r#const;
 pub type AlreadyCancelled = r#const::Const<true>;
 pub type NeverCancelled = r#const::Const<false>;
+
+mod atomic;
+pub use atomic::Atomic;

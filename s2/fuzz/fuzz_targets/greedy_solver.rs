f72ade7e45a8de96c77a3abc4a6911591425a5bc
Kostya <happy.boat9793@fastmail.com>
2025-06-15 15:40:12 +0000

    find hard examples with fuzzing

--- s2/fuzz/fuzz_targets/greedy_solver.rs
+++ s2/fuzz/fuzz_targets/greedy_solver.rs
@@ -2,11 +2,31 @@
 #[macro_use]
 extern crate libfuzzer_sys;
 extern crate s2;
-use s2::cancellation_flag::NeverCancelled;
+use s2::cancellation_flag::Atomic;
 use s2::format::{read_from_string, RowMajorAscii};
-use s2::grid::{ArrGridRowMajor, GridMutWithDefault};
-use s2::solver::{GreedySolver, Solver};
+use s2::grid::{ArrGridRowMajor, Grid, GridDiff, GridMutWithDefault};
+use s2::solver::{GreedySolver, Solver, SolverError};
 use s2::status::{eval_status, SudokuStatus};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+fn solve_with_timeout<T>(grid: &T, timeout: Duration) -> Result<Vec<GridDiff>, SolverError>
+where
+    T: Grid,
+{
+    let grid = ArrGridRowMajor::copy_of(grid);
+    let cancel = Arc::new(Atomic::new());
+    let cancellation_flag = cancel.clone();
+    let solve = thread::spawn(move || {
+        let cancellation_flag = cancellation_flag.clone();
+        GreedySolver::new().solve::<_, _, Vec<_>>(cancellation_flag.as_ref(), &grid)
+    });
+    // Before introduction of naked singles this example used to timeout.
+    thread::sleep(timeout);
+    cancel.cancel();
+    solve.join().unwrap()
+}
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(grid) = str::from_utf8(data) {
@@ -17,9 +37,8 @@ fuzz_target!(|data: &[u8]| {
                 match status {
                     SudokuStatus::Complete => (),
                     SudokuStatus::Incomplete => {
-                        match GreedySolver::new()
-                            .solve::<_, _, Vec<_>>(&NeverCancelled::new(), &grid)
-                        {
+                        match solve_with_timeout(&grid, Duration::from_secs(5)) {
+                            Err(SolverError::Cancelled) => panic!("timed out\n{:?}", grid),
                             Err(_) => (),
                             Ok(diff) => {
                                 let complete = ArrGridRowMajor::with_diff(&grid, diff.into_iter());
